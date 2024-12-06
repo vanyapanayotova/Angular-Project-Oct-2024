0cce3daa38cbf987491807a81a0c7a4feafe850d
vanyapanayotova <v.panayotova@yahoo.com>
2024-12-06 23:19:31 +0200

    add bootstrap components

--- workshop/src/app/app.component.ts
+++ workshop/src/app/app.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { CatalogComponent } from './catalog/catalog.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    CatalogComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
