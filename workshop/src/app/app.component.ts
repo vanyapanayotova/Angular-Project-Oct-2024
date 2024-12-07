d95b5709f52f3e5c02b8d174373854c97c877808
vanyapanayotova <v.panayotova@yahoo.com>
2024-12-07 19:48:07 +0200

    design

--- workshop/src/app/app.component.ts
+++ workshop/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { CatalogComponent } from './catalog/catalog.component';
+import { BannerComponent } from './banner/banner.component';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +11,8 @@ import { CatalogComponent } from './catalog/catalog.component';
     RouterOutlet,
     HeaderComponent,
     FooterComponent,
-    CatalogComponent
+    CatalogComponent,
+    BannerComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
